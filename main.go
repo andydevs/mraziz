1221d92219f11a371c0bacd45afa8b8f9d508b54
Anshul Kharbanda <akanshul97@gmail.com>
2025-02-22 20:17:44 +0000

    Create init cmd

--- main.go
+++ main.go
@@ -1,46 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"embed"
-	"os"
-	"text/template"
-)
-
-type Person struct {
-	Name      string
-	Adjective string
-}
-
-//go:embed res/*
-var efs embed.FS
+import "github.com/andydevs/mraziz/cmd"
 
 func main() {
-	// Read embedded file
-	tmplfileBytes, err := efs.ReadFile("res/test.tmpl")
-	if err != nil {
-		panic(err)
-	}
-	tmplFileText := string(tmplfileBytes)
-
-	// Create template
-	tmpl, err := template.New("test.tmpl").Parse(tmplFileText)
-	if err != nil {
-		panic(err)
-	}
-
-	// Print examples
-	people := []Person{
-		{"Parker", "late"},
-		{"Megan", "high"},
-		{"Gordon", "squishy"},
-		{"Harry", "hairy"},
-		{"Dan", "the man"},
-	}
-	for _, person := range people {
-		err = tmpl.Execute(os.Stdout, person)
-		if err != nil {
-			panic(err)
-		}
-	}
-
+	cmd.Execute()
 }
