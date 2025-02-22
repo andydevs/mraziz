c9abf91efcb69d44c88d552562100a13681b0d74
Anshul Kharbanda <akanshul97@gmail.com>
2025-02-22 03:28:45 +0000

    Create basic template setup (including embedded files)

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"embed"
+	"os"
+	"text/template"
 )
 
+type Person struct {
+	Name      string
+	Adjective string
+}
+
+//go:embed res/*
+var efs embed.FS
+
 func main() {
-	fmt.Println("PARKER YOU'RE LATE MAN ALWAYS LATE")
+	// Read embedded file
+	tmplfileBytes, err := efs.ReadFile("res/test.tmpl")
+	if err != nil {
+		panic(err)
+	}
+	tmplFileText := string(tmplfileBytes)
+
+	// Create template
+	tmpl, err := template.New("test.tmpl").Parse(tmplFileText)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print examples
+	people := []Person{
+		{"Parker", "late"},
+		{"Megan", "high"},
+		{"Gordon", "squishy"},
+		{"Harry", "hairy"},
+		{"Dan", "the man"},
+	}
+	for _, person := range people {
+		err = tmpl.Execute(os.Stdout, person)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 }
